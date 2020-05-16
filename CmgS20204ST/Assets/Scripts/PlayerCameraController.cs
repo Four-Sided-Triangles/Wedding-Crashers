9eb2a3d2b1ceb3f1497ebd99eb152a72a5169984
Claudia <claudia.lapalme@gmail.com>
2020-05-15 23:15:29 -0400

    Player commands
    - snappier player commands
    - ability to increase and decrease speed

--- CmgS20204ST/Assets/Scripts/PlayerCameraController.cs
+++ CmgS20204ST/Assets/Scripts/PlayerCameraController.cs
@@ -3,7 +3,7 @@
 public class PlayerCameraController : MonoBehaviour
 {
     public Transform player;
-    public Vector3 offset;
+    private Vector3 offset = new Vector3(5.5f, 7f, -10f);
 
     // Update is called once per frame
     void Update()
---
+++ CmgS20204ST/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    private Rigidbody rb;
+    private float forwardSpeed = 1000f;
+    private float lateralSpeed = 2000f;
+
+    private void setRb()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.drag = 0.5f;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        setRb();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rb.AddForce(0, 0, Time.deltaTime * forwardSpeed);
+        playerMovement();
+        setSpeed();
+    }
+
+    private void playerMovement()
+    {
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(Time.deltaTime * -lateralSpeed, 0, 0);
+        }
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(Time.deltaTime * lateralSpeed, 0, 0);
+        }
+    }
+
+    private void setSpeed()
+    {
+        if (Input.GetKey("i"))
+        {
+            forwardSpeed = 500f;
+        }
+        else if (Input.GetKey("o"))
+        {
+            forwardSpeed = 1000f;
+        }
+        else if (Input.GetKey("p"))
+        {
+            forwardSpeed = 1500f;
+        }
+    }
+}
