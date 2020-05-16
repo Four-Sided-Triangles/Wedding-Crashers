176f2b20e4ef7eb23c5422eb558a224b14fbe57c
Armando Russo <russo_449@hotmail.com>
2020-05-16 15:39:00 -0400

    Add Timer script

---
+++ CmgS20204ST/Assets/Scripts/Timer.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Timer : MonoBehaviour
+{
+    public float timer = 120f;
+    private GameObject player;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("decrement", 2f, 1f); // Arguments: method name, call this method at x time, call this method every y time
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void decrement()
+    {
+        if (timer == 0)
+        {
+            Debug.Log("Game Over!");
+
+        }
+        timer--;
+    }
+
+}
