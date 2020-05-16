8e19be92c814f84eccbadf751754766d196165aa
Claudia <claudia.lapalme@gmail.com>
2020-05-16 13:15:59 -0400

    Finished main menu

--- CmgS20204ST/Assets/Scripts/MainMenu.cs
+++ CmgS20204ST/Assets/Scripts/MainMenu.cs
@@ -1,17 +1,45 @@
 ﻿using UnityEngine;
 using UnityEngine.SceneManagement;
+using System.Collections;
 
 public class MainMenu : MonoBehaviour
 {
+
+    public AudioSource source;
+    public AudioClip hoverSound;
+    public AudioClip clickSound;
+
+    public void onHover()
+    {
+        source.PlayOneShot(hoverSound);
+    }
+
+    public void onClick()
+    {
+        source.PlayOneShot(clickSound);
+    }
+
     public void PlayGame()
     {
+        StartCoroutine(DelaySceneLoad());
+    }
+
+    public IEnumerator DelaySceneLoad()
+    {
+        yield return new WaitForSeconds(clickSound.length - 0.25f);
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public IEnumerator DelayQuit()
+    {
+        yield return new WaitForSeconds(clickSound.length - 0.25f);
+        Application.Quit();
     }
 
     public void QuitGame()
     {
         Debug.Log("DOBBY IS FREEEEEEE!!!");
-        Application.Quit();
+        DelayQuit();
     }
 
 }
