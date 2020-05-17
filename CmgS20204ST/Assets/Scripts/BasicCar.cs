680b882f90579cbae2873354187fd53763a3850d
Daniel Wiktorczyk <danielwiktorczyk@gmail.com>
2020-05-17 13:04:01 -0400

    Destroy Cars if Idle

--- CmgS20204ST/Assets/Scripts/BasicCar.cs
+++ CmgS20204ST/Assets/Scripts/BasicCar.cs
@@ -9,17 +9,32 @@ public class BasicCar : MonoBehaviour
     public float forwardSpeed;
     public float maxSpeed;
     public bool isHit = false;
+    public Vector3 previousPosition;
+    const float toleranceForDeletingStoppedObjects = 0.001f;
 
     // Start is called before the first frame update
     void Start()
     {
+        InvokeRepeating("DestroyIfIdle", 3f, .1f);
         rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
 
-    private void Update()
+    private void DestroyIfIdle()
     {
+        if(gameObject.transform.position.x < previousPosition.x + toleranceForDeletingStoppedObjects
+            && gameObject.transform.position.y < previousPosition.y + toleranceForDeletingStoppedObjects
+            && gameObject.transform.position.z < previousPosition.z + toleranceForDeletingStoppedObjects)
+        {
+            Destroy(gameObject);
+        }
+
+        previousPosition = gameObject.transform.position;
+    }
+
+    private void Update()
+    {   
         if (isHit == false)
         {
             transform.localRotation = Quaternion.Euler(-90f, 0f, 0f);
