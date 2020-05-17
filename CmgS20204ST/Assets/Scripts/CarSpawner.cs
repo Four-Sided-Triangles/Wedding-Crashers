a192e195ac2679b5e6c420e5219296259e31fff0
Daniel Wiktorczyk <danielwiktorczyk@gmail.com>
2020-05-17 11:40:45 -0400

    WIP Spawner
    Weird ass camera problem

---
+++ CmgS20204ST/Assets/Scripts/CarSpawner.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarSpawner : MonoBehaviour
+{
+    public GameObject basicCar;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("Spawn", 0f, 3f); // Arguments: method name, call this method at x time, call this method every y time
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Spawn()
+    {
+        // Instantiate at position (0, 0, 0) and zero rotation.
+        Instantiate(basicCar, gameObject.transform.position, Quaternion.identity);
+    }
+}
