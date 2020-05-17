b646688393a34f8bf90a416c4d7855341ce3fee8
Daniel Wiktorczyk <danielwiktorczyk@gmail.com>
2020-05-17 12:34:52 -0400

    Update Spawner, Adjust Mass of Cars

--- CmgS20204ST/Assets/Scripts/CarSpawner.cs
+++ CmgS20204ST/Assets/Scripts/CarSpawner.cs
@@ -4,23 +4,58 @@ using UnityEngine;
 
 public class CarSpawner : MonoBehaviour
 {
-    public GameObject basicCar;
+    public GameObject classicCar;
+    public GameObject familyCar;
+    public GameObject iceCreamTruck;
+    public GameObject sportsCar;
+    public GameObject truck;
+
+    public float spawnCoolDown;
 
     // Start is called before the first frame update
     void Start()
     {
-        InvokeRepeating("Spawn", 0f, 3f); // Arguments: method name, call this method at x time, call this method every y time
+        Random.InitState((int) Time.time);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (spawnCoolDown <= 0)
+        {
+            Invoke("Spawn", 0f);
+            spawnCoolDown = Random.Range(180f, 500f);
+        }
+        else
+        {
+            spawnCoolDown--;
+        }
+
     }
 
     void Spawn()
     {
         // Instantiate at position (0, 0, 0) and zero rotation.
-        Instantiate(basicCar, gameObject.transform.position, Quaternion.identity);
+        int ran = (int) Random.Range(0, 4);
+        GameObject selectedCar = familyCar;
+        switch (ran)
+        {
+            case 0:
+                selectedCar = classicCar;
+                break;
+            case 1:
+                selectedCar = familyCar;
+                break;
+            case 2:
+                selectedCar = iceCreamTruck;
+                break;
+            case 3:
+                selectedCar = sportsCar;
+                break;
+            case 4:
+                selectedCar = truck;
+                break;
+        }
+        Instantiate(selectedCar, gameObject.transform.position, Quaternion.identity);
     }
 }
